c7bb69228d9db3d6347214c7b4cb87d24b89f382
James Royston <purpleposeidon@gmail.com>
2010-08-04 23:45:34 -0700

    fancier ./install script

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 from distutils.core import setup
 
 version = open('VERSION').read().strip()
@@ -20,3 +19,18 @@ setup(
   packages=['terminal'],
 )
 
+#import sys
+#
+#if sys.version_info[0] == 3:
+#  #tell the peep about our fancy codecs module
+#  import codecs, fixed_codecs
+#  oldversion = True
+#  if hasattr(codecs, "_codecs_hack_version"):
+#    if codecs._codecs_hack_version == fixed_codecs._codecs_hack_version:
+#      oldversion = False
+#    else:
+#      "is old."
+#  else:
+#    state = "not installed."
+#  if oldversion:
+#    print("The codecs library that comes with python3 has a bug that breaks ")
